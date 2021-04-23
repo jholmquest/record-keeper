d3a11a61d4b0a3da62fa068329e488047c9a5d86
James Holmquest <jpholmquest@gmail.com>
2021-04-22 21:50:00 -0500

    moved read to a class

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
     require_once('template/header.php');
     require_once('template/footer.php');
-    require_once('resources/properties.php');
-    require_once('Character.php');
+    require_once('characterCRD.php');
 
-$db = new PDO(DB_SCHEMA, DB_USER, DB_PASS);
-            
-// Read all records
-$sql = "SELECT * FROM game_character";
+    $reader = new CharacterCRD();
 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-try
-{
-    $query = $db->prepare($sql);
-    $query->execute();
-    $results = $query->fetchAll(PDO::FETCH_CLASS, 'Character');
-}
-catch (Exception $ex)
-{
-    echo "{$ex->getMessage()}<br/>";
-}
-
-foreach($results as $character)
-{
-    echo $character;
-}
+    $reader->readAll();
 ?>

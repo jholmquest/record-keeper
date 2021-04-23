d58c3293b023a845074bcfc174907b25eec19376
James Holmquest <jpholmquest@gmail.com>
2021-04-22 21:18:46 -0500

    magic getter/setter

--- Character.php
+++ Character.php
@@ -4,6 +4,18 @@
         private $id;
         private $name;
         private $feats;
+
+        // Magic Get/Set
+        public function __get($ivar)
+        {
+            return $this->$ivar;
+        }
         
+        public function __set($ivar, $value)
+        {
+            $this->$ivar = $value;
+        }
+
+
     }
 ?>

f3edb2edf2c7d875e44a1ca94f2de6584a8f2e89
James Holmquest <jpholmquest@gmail.com>
2021-04-28 16:18:35 -0500

    added more to character page, created CRUD class

--- character.php
+++ character.php
@@ -1,6 +1,19 @@
 <?php
     session_start();
+
+    // forces user to home page if there is no session
+    if (!isset($_SESSION['id'])) {
+        header('Location: .');
+        exit;
+    }
+
+    require_once('template/header.php');
+
+
     echo $_SESSION['id'];
     echo "<br>";
-    echo $_SESSION['character'];
+    echo "<h2>Feats for " . $_SESSION['character'] . "</h2>";
+
+    echo "<a href='.'>return</a>";
+    require_once('template/footer.php');
 ?>
--- template/header.php
+++ template/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Record Keeper</title>
 </head>
 <body>
     <header>
-        <h1><?php echo "Welcome to the Record Keeper"; ?></h1>
+        <h1>Record Keeper</h1>
     </header>
     <main>

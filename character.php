10b954f3d8d5244fc7a5f3c4688452560de5f81f
James Holmquest <jpholmquest@gmail.com>
2021-04-28 16:37:56 -0500

    created form

--- character.php
+++ character.php
@@ -14,9 +14,29 @@
     $feat_reader = new FeatCRUD();
     $feats = $feat_reader->readAll($_SESSION['id']);
 
+    echo "<table>";
+
     foreach($feats as $feat) {
-        echo $feat;
+
+        echo "<tr><td>$feat</td>";
+        echo "<td><a href='#'>edit</a></td>";
+        echo "<td><a href='#'>delete</a><td></tr>";
     }
+
+    echo "</table>";
+?>
+    <h3>Add a new feat</h3>
+    <form name='featCreator' action='#' method='POST'>
+        <label for='featName'>Feat Name</label>
+        <input type='text' id='featName' name='featName' required>
+
+        <label for='featLink'>URL</label>
+        <input type='text' id='featLink' name='featLink' required>
+
+        <button type='submit' name='addFeat'>Add Feat</button>
+    </form>
+<?php
+
 
     echo "<a href='.'>return</a>";
     require_once('template/footer.php');

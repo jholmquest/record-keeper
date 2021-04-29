2a84af288c5b0d689096f06b683bfc4231b25232
James Holmquest <jpholmquest@gmail.com>
2021-04-28 22:51:33 -0500

    showing feedback

--- deleteFeat.php
+++ deleteFeat.php
@@ -1,12 +1,19 @@
 <?php
     session_start();
-
     if (isset($_POST['deleteFeat'])) {
 
         require_once('featDAO.php');
+        require_once('template/header.php');
 
-        $feat_dao->delete($_POST['deleteFeat'], $_SESSION['id']);
+        $rows_affected = $feat_dao->delete($_POST['deleteFeat'], $_SESSION['id']);
+
+        echo "rows deleted: $rows_affected <br>";
+        echo "<a href='character.php'>view character</a>";
+
+        require_once('template/footer.php');
+    } else {
+
+        header('Location: character.php?id');
+        exit;
     }
-    header('Location: character.php?id');
-    exit;
 ?>

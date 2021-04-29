51f9719cc431affb1b125e2b1944137de8fc811a
James Holmquest <jpholmquest@gmail.com>
2021-04-29 10:52:53 -0500

    added code for displaying edit form

--- editFeat.php
+++ editFeat.php
@@ -1,4 +1,28 @@
 <?php
-    header('Location: character.php?edit=true');
-    exit;
+
+    session_start();
+    if (isset($_GET['editFeat'])) {
+
+        require_once('template/header.php');
+        require_once('featDAO.php');
+        $feat = $feat_dao->readById($_GET['editFeat']);
+?>
+        <form action='editFeat.php' method='POST'> 
+        <label for='featName'>Feat Name</label>
+        <input type='text' id='featName' name='featName' value='<?php echo $feat->feat_name; ?>' required>
+
+        <label for='featLink'>URL</label>
+        <input type='text' id='featLink' name='featLink' value='<?php echo $feat->feat_url; ?>'required>
+
+        <input type='hidden' name='featID' value='<?php echo $feat->id; ?>'>
+        <button type='submit' name='updateFeat'>Update</button>
+        </form>
+<?php
+        
+        
+        require_once('template/footer.php');
+    } else {
+        header('Location: character.php?edit=false');
+        exit;
+    }
 ?>

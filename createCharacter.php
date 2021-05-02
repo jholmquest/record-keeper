a993904e78e1c7be48da5bc1fffa9019dfd9fb3b
James Holmquest <jpholmquest@gmail.com>
2021-05-02 16:30:43 -0500

    deleted methods that are no longer needed

--- createCharacter.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (isset($_POST['create'])) {
-        require_once('characterDao.php');
-
-        $character_dao->create($_POST['characterName']);
-    }
-    header('Location: .');
-    exit;
-?>
